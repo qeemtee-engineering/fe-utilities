ea99fdc8314cca5566bb0479668abb1390cbe243
Faizan Baig <baigfaizan95@gmail.com>
2019-10-01 16:56:14 +0530

    new build

--- src/lib/helpers/s3Utils.js
+++ src/lib/helpers/s3Utils.js
@@ -1,7 +1,7 @@
 export const replaceS3withCDN = url => {
-  const s3URL = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com`;
+  const s3URL = `https://${process.env.REACT_APP_AWS_BUCKET_NAME}.s3.amazonaws.com`;
   if (url.includes(s3URL)) {
-    url = url.replace(s3URL, process.env.AWS_CDN_NAME);
+    url = url.replace(s3URL, process.env.REACT_APP_CDN_NAME);
   }
   return url;
 };
