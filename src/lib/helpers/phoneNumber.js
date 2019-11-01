6a0b800b153a978fe562bf562e7c82055c07a7d7
Ali Raza Sherazi <syedd.sherazi@gmail.com>
2019-11-01 10:03:13 +0500

    Phone number util minor change

--- src/lib/helpers/phoneNumber.js
+++ src/lib/helpers/phoneNumber.js
@@ -1,12 +1,14 @@
 import get from 'lodash/get';
 
-export const getPhoneNumber = (data) => {
+export const getPhoneNumber = data => {
   if (get(data, 'phoneNumber')) {
     return `+${data.phoneNumber.countryCode}-${data.phoneNumber.nationalNumber}`;
   } else if (get(data, 'countryCode') && get(data, 'phone')) {
     return `+${data.countryCode}-${data.phone}`;
   } else if (get(data, 'phone')) {
-        return `${data.phone}`;
-    }
+    return `${data.phone}`;
+  } else if (get(data, 'countryCode') && get(data, 'nationalNumber')) {
+    return `+${data.countryCode}-${data.nationalNumber}`;
+  }
   return 'N/A';
 };
