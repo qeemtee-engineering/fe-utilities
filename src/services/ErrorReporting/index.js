8448324544163f0eacd098f9dd68b1d268dda6b7
Faizan Baig <baigfaizan95@gmail.com>
2020-02-06 17:55:06 +0530

    removed sentry and unused files

--- src/services/ErrorReporting/index.js
+++
@@ -1,1 +0,0 @@
-export { SentryErrorReporting } from './Providers';
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,1 @@
-export { default as ErrorReportingService } from './ErrorReporting.service';
 export { default as AnalyticsService } from './Analytics.service';
-export * from './ErrorReporting';
