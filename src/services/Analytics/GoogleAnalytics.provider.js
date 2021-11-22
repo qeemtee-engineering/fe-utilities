389050993bb8dd035e4e820b81f2596ff0f90091
Faizan Baig <admin@qeemtee.com>
2021-11-23 04:00:53 +0530

    new build

--- src/services/Analytics/GoogleAnalytics.provider.js
+++ src/services/Analytics/GoogleAnalytics.provider.js
@@ -1,37 +1,43 @@
-import ua from 'universal-analytics';
+import ua from "universal-analytics";
 
 export default class GoogleAnalytics {
   constructor(token) {
-    const isRelease = process.env.NODE_ENV === 'production';
-    if (!isRelease || token === undefined) {
+    const isRelease = process.env.NODE_ENV === "production";
+    if (!isRelease || !token) {
       console.warn(
-        'Missing token for GA. Set the GA_TOKEN env var correctly to configure analyitcs'
+        "Missing token for GA. Set the GA_TOKEN env var correctly to configure analyitcs"
       );
       return;
     }
     this.token = token;
     this.visitor = ua(token);
-    console.info('Google Analytics has been initialized');
+    console.info("Google Analytics has been initialized");
   }
 
   trackPage(pageName) {
-    this.visitor.pageview(pageName).send();
+    if (this.visitor) {
+      this.visitor.pageview(pageName).send();
+    }
   }
 
   trackEvent(category, action) {
-    this.visitor.event(category, action).send();
+    if (this.visitor) {
+      this.visitor.event(category, action).send();
+    }
   }
 
   register(userDetails) {
-    if (userDetails.id) {
-      this.visitor = ua(this.token, userDetails.id, {
-        strictCidFormat: false,
-        http: true
+    if (this.visitor) {
+      if (userDetails.id) {
+        this.visitor = ua(this.token, userDetails.id, {
+          strictCidFormat: false,
+          http: true,
+        });
+        delete userDetails.id;
+      }
+      Object.keys(userDetails).forEach((key) => {
+        this.visitor.set(key, userDetails[key]);
       });
-      delete userDetails.id;
     }
-    Object.keys(userDetails).forEach(key => {
-      this.visitor.set(key, userDetails[key]);
-    });
   }
 }
